2788832fab3a847a1be31f84bf81ee665c4aa3c1
Marcus <88283829+PyMarcus@users.noreply.github.com>
2022-07-27 17:00:26 -0400

    5 como produtp

--- 5.asm
+++ 5.asm
@@ -21,13 +21,13 @@
 	move $t1, $v0 
 	
 	li $t2, 0 # contador
-	li $t3, 0 # soma
+	li $t3, 1 # produto
 	loop:
 		# soma os valores até o numero, se o valor for igual ao número
 		# este será um triângular
 		
 		addi $t2, $t2, 1
-		add $t3, $t2, $t3
+		mul $t3, $t2, $t3
 		beq $t3, $t1, yep  # se for igual, vai para o rotulo
 	ble $t2, $t1, loop 
 	li $v0, 4
